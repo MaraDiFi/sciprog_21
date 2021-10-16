e2e197f8b559532c60b87d972567db3a58d92836
MaraDiFi <91842759+MaraDiFi@users.noreply.github.com>
2021-10-16 16:39:11 +0100

    uploading folder with codes required for practical3

---
+++ Practical03/Printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+/*setting variables*/
+int i1=168.19, i2=267.14, i3= 49;
+
+/*setting floats*/
+float fl1=1.11, fl2=1.111, fl3=1.1;
+
+/*command for showing the values*/
+printf ("Integers are three %d, %d, %.d and the floats are %.3f, %.3f, %.1f", i1, i2, i3, fl1, fl2, fl3);
+
+return 0;
+}
+~
+~
+
